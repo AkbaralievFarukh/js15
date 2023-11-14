6975720060220872518c6a6ff02cb1ba8690e6cf
Farukh <akbfarukh@gmail.com>
2023-11-14 16:36:44 +0600

    modifided

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-const email = document.querySelector('#input-email').value;
-const lastName = document.querySelector('#input-last-name').value;
-const firstName = document.querySelector('#input-first-name').value;
-const userName = document.querySelector('#input-user-name').value;
-const password = document.querySelector('#password').value;
-const confirmPassword = document.querySelector('#confirm-password').value;
-const formUser = document.querySelector('#form-user')
+function validateForm() {
+    const username = document.getElementById('username').value;
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
 
-formUser.addEventListener('submit', () => {
-    if (
-        email.length > 2 &&
-        lastName.length > 2 &&
-        (password.length > 2 && password.length < 8) &&
-        (confirmPassword.length > 2 && confirmPassword.length < 8) &&
-        firstName.length > 2 && userName.length > 2
-        && password === confirmPassword
-    ) {
-        alert('Успешная регистрация');
+    if (username.length < 3
+        || firstName.length < 3
+        || lastName.length < 3
+        || password.length < 3
+        || password.length > 8
+        || confirmPassword.length < 3
+        || confirmPassword.length > 8) {
+        alert('Пожалуйста, убедитесь, что все поля соответствуют требованиям.');
+    } else if (password !== confirmPassword) {
+        alert('Пароль и подтверждение пароля не совпадают.');
     } else {
-        alert('Проверьте правильность полей');
+        alert('Форма успешно отправлена!\n\nUser Name: ' + username + '\nFirst Name: ' + firstName + '\nLast Name: ' + lastName + '\nEmail: ' + email);
     }
-})
-
+}
